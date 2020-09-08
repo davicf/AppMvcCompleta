e6191ca975571f37f3c2d4b2412b9b5731b68cd7
Davi Carvalho <davi_carvalho1@live.com>
2020-09-08 11:35:43 -0300

    feat: adição do contexto e os mapeamentos dos modelos

--- src/Dev.App/Startup.cs
+++ src/Dev.App/Startup.cs
@@ -12,6 +12,7 @@ using Dev.App.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Dev.Data.Context;
 
 namespace Dev.App
 {
@@ -27,11 +28,15 @@ namespace Dev.App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<MeuDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
