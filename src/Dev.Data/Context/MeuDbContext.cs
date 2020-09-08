9b91eeae3e48dab27e9d2d5bdc49f39c67825c89
Davi Carvalho <davi_carvalho1@live.com>
2020-09-08 12:11:58 -0300

    feat: adição das migrations

--- src/Dev.Data/Context/MeuDbContext.cs
+++ src/Dev.Data/Context/MeuDbContext.cs
@@ -22,7 +22,7 @@ namespace Dev.Data.Context
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
 
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
-                relationship.DeleteBehavior = DeleteBehavior.SetNull;
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
             base.OnModelCreating(modelBuilder);
         }
